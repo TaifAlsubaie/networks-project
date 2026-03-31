cab17cad1c6d8210ecd7f60f96ab7e15674b45b2
Taif Alsubaie <taif.majed.alsubaie@gmail.com>
2026-03-31 17:18:54 +0300

    add header,footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>© 2026 Network Project | All Rights Reserved</p>
+</footer>
+
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+
+    <title>Network Project</title>
+    
+    <!-- Main Style -->
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+
+<header>
+    <nav class="navbar">
+        <h2>network project</h2>
+
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="ethernet.php">Ethernet</a></li>
+            <li><a href="wireless.php">Wireless</a></li>
+            <li><a href="comparison.php">Comparison</a></li>
+            <li><a href="speedtest.php">Speed Test</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+            <li><a href="login.php"><i class="fa-solid fa-user"></i></a></li>
+        </ul>
+    </nav>
+</header>
